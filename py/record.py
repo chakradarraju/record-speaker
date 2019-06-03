d11645d58056932463a77b1a1290a69a29f69218
Srikar Yekollu <srikar@strings.ai>
2019-06-03 18:04:01 +0530

    fix record to store 16 bit pcm wav

--- py/record.py
+++ py/record.py
@@ -9,19 +9,22 @@ lb = sc.all_microphones(include_loopback=True)[1]
 print(lb)
 data = []
 i = 0
-with lb.recorder(samplerate=44100) as rcdr, wave.open('recorded.wav', 'wb') as f:
-    f.setnchannels(2)
-    f.setsampwidth(2)
-    f.setframerate(44100)
+with lb.recorder(samplerate=44100) as rcdr:
+   # , wave.open('recorded.wav', 'wb') as f:
+   # f.setnchannels(2)
+   # f.setsampwidth(2)
+   # f.setframerate(44100)
 
     while i < 100:
         d = rcdr.record()
-        print(d.shape)
         data.append(d)
         i += 1
     final = np.concatenate(data, axis=0)
     print(final.shape)
-    scipy.io.wavfile.write("recorded2.wav", 44100, final)
+    print(final.dtype)
+    finalint = final.astype(np.int16)
+    print(finalint.dtype)
+    scipy.io.wavfile.write("recorded2.wav", 44100, finalint)
 
 
 
